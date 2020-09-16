7cdb04bbee3e74256208fc5a0e036cefc1defa45
Piotr Kmita <piotr@szkicownik.com>
2020-09-16 17:54:57 +0200

    Add plugins in webpack.dev.js
    Add CleanWebpackPlugin and HtmlWebpackPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -30,4 +32,12 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      inject: true,
+    })   
+  ] 
 }
