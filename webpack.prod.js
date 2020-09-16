79197f92197ef00e143bef008f029806497b9680
Piotr Kmita <piotr@szkicownik.com>
2020-09-16 20:04:46 +0200

    Add plugins in webpack.prod.js
    Add MiniCssExtractPlugin and HtmlWebpackPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports =  {
   mode: "production",
@@ -6,6 +10,12 @@ module.exports =  {
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, "./dist")
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin(),
+      new TerserPlugin()
+    ]
   },
   module: {
     rules: [
@@ -30,4 +40,15 @@ module.exports =  {
       },
     ],
   }, 
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.bundle.css',
+      path: path.resolve(__dirname, './dist'),
+    }), 
+    new HtmlWebpackPlugin({      
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      inject: true
+    })      
+  ]
 }
